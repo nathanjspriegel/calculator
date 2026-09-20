75126e581babfe876f9907e0dbd31d57b3c6e348
Nathan Spriegel <250429027+nathanjspriegel@users.noreply.github.com>
2026-09-20 19:48:07 -0400

    Add operate function with basic arithmetic operations

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
-function add() {
+let a = 4;
+let b = 3;
+let operator = '+';
 
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+function operate(operator, a, b) {
+    switch (operator) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+        default:
+            return "Error";
+    }
 }
 
-function subtract() {
+let result = operate(operator, a, b);
+console.log(result);
 
-}
-
-function multiply() {
-
-}
-
-function divide() {
-
-}
