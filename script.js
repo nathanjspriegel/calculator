b3362f86ffb6556306e2ce410e444789a4ba4b8a
Nathan Spriegel <250429027+nathanjspriegel@users.noreply.github.com>
2026-09-20 22:40:29 -0400

    Add calc logic: input, operators, chaining, clear, delete
    Working digit, operator, equals, delete, and clear buttons
    with operate(). Fixed chaining so calculator does not evaluate
    more than a single pair of numbers at a time.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-let a = 4;
-let b = 3;
-let operator = '+';
+let a = 0;
+let b = 0;
+let operator = '';
+
 
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
@@ -13,15 +14,66 @@ function operate(operator, a, b) {
             return add(a, b);
         case '-':
             return subtract(a, b);
-        case '*':
+        case 'x':
             return multiply(a, b);
-        case '/':
+        case '÷':
             return divide(a, b);
         default:
             return "Error";
     }
 }
 
-let result = operate(operator, a, b);
-console.log(result);
+const display = document.querySelector('.display');
+const digitBtns = document.querySelectorAll('.digitBtn');
+const operatorBtns = document.querySelectorAll('.operatorBtn');
+const equalBtn = document.querySelector('#equalBtn');
+const deleteBtn = document.querySelector('#deleteBtn');
+const clearBtn = document.querySelector('#clearBtn');
+const decimal = document.querySelector('#decimalBtn');
 
+
+let input = '';
+
+digitBtns.forEach(button => {
+    button.addEventListener('click', () => {
+        input = input + button.textContent;
+        display.textContent = input;
+    });
+});
+
+operatorBtns.forEach(button => {
+    button.addEventListener("click", () => {
+        if (operator != '') {
+            a = Number(a);
+            b = Number(input);
+            let result = operate(operator, a, b);
+            display.textContent = result;
+            a = result;
+        } else {
+            a = input;
+        }
+        input = '';
+        operator = button.textContent;
+    });
+});
+
+equalBtn.addEventListener("click", () => {
+    a = Number(a);
+    b = Number(input);
+    input = '';
+    let result = operate(operator, a, b);
+    display.textContent = result;
+});
+
+deleteBtn.addEventListener('click', () => {
+    input = input.slice(0, -1);
+    display.textContent = input;
+});
+
+clearBtn.addEventListener('click', () => {
+    a = 0;
+    b = 0;
+    input = '';
+    operator = '';
+    display.textContent = '';
+});
